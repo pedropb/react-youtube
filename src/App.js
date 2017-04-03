5fc3cabe4f580030407b7c1c0cb8fb439fbc0543
Pedro Baracho <pedropbaracho@gmail.com>
2017-04-03 09:04:04 -0300

    Added VideoDetail Component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import YTSearch from 'youtube-api-search';
-import SearchBar from './components/search_bar'
+import SearchBar from './components/search_bar';
+import VideoDetail from './components/video_detail';
 
 const API_KEY='AIzaSyDA8-XYaf3o7R_gZB2z4Hqc2LPgWJ5TD2A';
 
@@ -8,15 +9,20 @@ class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { vidoes : [] }
+    this.state = { videos : [] };
 
-    YTSearch({key: API_KEY, term: 'New Zealand'}, function (data) {
+    YTSearch({key: API_KEY, term: 'New Zealand'}, (videos) => {
       this.setState({ videos });
     });
   }
 
   render() {
-    return <SearchBar />;
+    return (
+      <div>
+        <SearchBar />
+        <VideoDetail video={this.state.videos[0]} />
+      </div>
+    );
   }
 
 
