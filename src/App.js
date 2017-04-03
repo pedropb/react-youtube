2d20e84e13f6433c0790368819883682fb6302d7
Pedro Baracho <pedropbaracho@gmail.com>
2017-04-03 08:44:39 -0300

    Implemented YTSearch API call and set state to App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar'
 
-const App = () => {
-  return <SearchBar />;
+const API_KEY='AIzaSyDA8-XYaf3o7R_gZB2z4Hqc2LPgWJ5TD2A';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { vidoes : [] }
+
+    YTSearch({key: API_KEY, term: 'New Zealand'}, function (data) {
+      this.setState({ videos });
+    });
+  }
+
+  render() {
+    return <SearchBar />;
+  }
+
+
 }
 
 export default App;
