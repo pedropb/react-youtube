782883a86cb8c067701daa234d3cc71731698ed1
Pedro Baracho <pedropb@trt3.jus.br>
2017-04-04 18:33:45 -0300

    Implemented VideoList and VideoListItem

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React, { Component } from 'react';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoDetail from './components/video_detail';
+import VideoList from './components/video_list';
 
 const API_KEY='AIzaSyDA8-XYaf3o7R_gZB2z4Hqc2LPgWJ5TD2A';
 
 class App extends Component {
+  
   constructor(props) {
     super(props);
 
@@ -21,6 +23,7 @@ class App extends Component {
       <div>
         <SearchBar />
         <VideoDetail video={this.state.videos[0]} />
+        <VideoList videos={this.state.videos} />
       </div>
     );
   }
