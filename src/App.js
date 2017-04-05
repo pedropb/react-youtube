05d92a5c1bafb7074c7fa4a63d4ddfa092f4e963
Pedro Baracho <pedropbaracho@gmail.com>
2017-04-04 23:22:51 -0300

    Styling complete

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoDetail from './components/video_detail';
 import VideoList from './components/video_list';
+import './App.css';
 
 const API_KEY='AIzaSyDA8-XYaf3o7R_gZB2z4Hqc2LPgWJ5TD2A';
 
